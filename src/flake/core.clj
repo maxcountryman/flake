ee95e9808a925c2ee1ac576e42c159b635963a7c
Max Countryman <maxc@me.com>
2016-03-16 10:07:15 -0700

    add convenience for ID generation

--- src/flake/core.clj
+++ src/flake/core.clj
@@ -145,3 +145,7 @@
         seq-no (next-seq-no! ts)
         flake  (flake-byte-buffer ts hardware-address seq-no)]
     (bigint (.array flake))))
+
+(def ^{:doc "Like generate but returns an infinite, lazy sequence of IDs."}
+  lazy-gen
+  (repeatedly generate))
