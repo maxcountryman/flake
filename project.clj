02f952c28ceecc559d07911c69900a4489d4c6cb
Max Countryman <maxc@me.com>
2016-05-31 19:06:27 -0700

    update project dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "https://github.com/maxcountryman/flake"
   :license {:name "BSD 3-Clause license"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [primitive-math "0.1.3"]]
-  :profiles {:dev {:dependencies [[criterium "0.4.3"]]}}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [primitive-math "0.1.5"]]
+  :java-source-paths ["src/flake"]
+  :profiles {:dev {:dependencies [[criterium "0.4.4"]]}}
   :test-selectors {:default   (complement :benchmark)
                    :benchmark :benchmark
                    :all       (constantly true)}
